d1cb9ba68ccabc76f2a61da7c373d54b89aad657
IMDAD <ahmedimdad1593@gmail.com>
2020-12-08 02:28:58 +0600

    graphql app frontend done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "https://imdads-reading-list.herokuapp.com/graphql",
+});
+
+// https://imdads-reading-list.herokuapp.com/
+// http://localhost:4000/graphql
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Imdad's Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
